3abc347f54e7440eb2b7f45b562c5f9989a8a366
Paulo Sérgio <paulo.araujo@gmail.com>
2018-07-26 17:09:34 -0300

    fixing python file

--- 7-oxygen/oxygen.py
+++ 7-oxygen/oxygen.py
@@ -1,37 +1,19 @@
-#coding: utf-8
-
-from Tkinter import *
-
-def getRGB(image, x, y):
-
-    value = image.get(x, y)
-
-    return value
+# coding: utf-8
+from PIL import Image
 
 def main():
-    photo = PhotoImage(file="oxygen.png")
-    cursor = 1
-    lastGray = 0
-    msg = ""
-    while (cursor < photo.width()):
-        tempLastGray = getRGB(photo, cursor, 47)[0]
-
-        lastGray = tempLastGray
-        msg += chr(lastGray)
-        cursor += 7
-
-    answer = [105, 110, 116, 101, 103, 114, 105, 116, 121]
-
-    print msg
-    print "Result = " + ''.join(map(chr, answer))
-
-    for e in answer: print e, chr(e)
-
-    # label = Label(root, image=photo)
-    # label.grid()
-    # root.mainloop()
+    img = Image.open('oxygen.png')
+    pix = img.load()
+    width, height = img.size
+    result = []
+    for i in range(3, width, 7):
+        result.append(chr(pix[i, height/2][0]))
+    print(''.join(result))
+    ans = [105, 110, 116, 101, 103, 114, 105, 116, 121]
+    print(''.join( map(chr, ans) ))
 
 
 
-root = Tk()
+
+#root = Tk()
 main()
